1e4b6e16be3aed7f2492b7730287d95926388441
italo-rabelo <italoalvesrabel@gmail.com>
2023-11-10 20:11:42 -0300

    Added script.py

---
+++ xml/script.py
@@ -0,0 +1,18 @@
+import xml.dom.minidom
+
+def get_xml():
+
+    doc = xml.dom.minidom.parse("file.xml");
+    root = doc.documentElement
+
+    tagName = doc.getElementsByTagName('hello')
+
+    print (doc.firstChild.tagName)
+    for music in tagName:
+        adele = music.getAttribute("adele")
+        tags = music.getElementsByName('item')[0].childNodes[0].data
+        print (adele)
+        print (tags)
+
+if __name__ == "__main__":
+    get_xml();
